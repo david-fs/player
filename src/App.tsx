a4ddee65807dd5772937dbb0575c778ee0dd748f
david-fs <firminosilva.david@gmail.com>
2021-11-13 20:56:46 -0300

    andamento

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,18 @@ import FramePlayer from "./components/FramePlayer/FramePlayer";
 
 
 function App() {
-    const imagesArray:string[] = [
-        "src/Images/1.jpg",
-        "src/Images/2.jpg",
-        "src/Images/3.jpg",
-        "src/Images/4.jpg",
-        "src/Images/5.jpg",
-        "src/Images/6.jpg"
+    const imagesArray:{ url: string }[] = [
+        {url:"https://live.staticflickr.com/65535/51677313191_0b2cd890e6.jpg"},
+        {url:"https://live.staticflickr.com/65535/51677560763_6986b9ce68.jpg"},
+        {url:"https://live.staticflickr.com/65535/51677984869_35a6fafd4f.jpg"},
+        {url:"https://live.staticflickr.com/65535/51677313136_1d62f66b5c.jpg"},
+        {url:"https://live.staticflickr.com/65535/51677560883_4b15402fe2.jpg"},
+        {url:"https://live.staticflickr.com/65535/51677560688_f54edde424.jpg"},
     ]
   return (
     <div className="App">
       <header className="App-header">
-        <FramePlayer  fps={0.2} frames={["1","2","3","4"]}/>
+        <FramePlayer  fps={1} frames={imagesArray}/>
       </header>
     </div>
   );
--- src/components/FramePlayer/FramePlayer.tsx
+++ src/components/FramePlayer/FramePlayer.tsx
@@ -1,25 +1,82 @@
-import React, { FC } from "react";
+import React, {FC, useEffect, useState} from "react";
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
+import {Slider} from "@mui/material";
 
-interface PropsPlayer<ValueType = any> {
-    frames: string[],
+interface PropsPlayer {
+    frames:any[],
     fps: number
 }
 
 const FramePlayer: FC<PropsPlayer> = ({frames, fps, ...props}) => {
+    const [framePorSegundo, setFramePorSegundo] = useState<number>(0)
+    const [tempoTotalDaApresentação, setTempoTotalDaApresentação] = useState<number>(0)
+    const [frame, setFrame] = useState<string>('')
+    const [onPlay, setOnPlay] = useState<boolean>(false)
+    const [time, setTime] = useState<number>(0)
+    const [progress, setProgress] = useState(0);
 
+    function definirTempos(){
+        let tempoDeTela:number;
+        let tempoTotal:number;
+
+        tempoDeTela = 1/fps;
+        setFramePorSegundo(tempoDeTela);
+
+        tempoTotal = frames.length * tempoDeTela;
+        setTempoTotalDaApresentação(tempoTotal)
+
+    }
+
+    useEffect(() => {
+        definirTempos()
+    });
+
+    useEffect(() => {
+        let timer: NodeJS.Timeout;
+        if (onPlay){
+             timer = setInterval(() => {
+                 const timeAtual = time
+                 setTime(timeAtual + 1)
+                 setFrame(frames[time].url)
+                 console.log(time);
+             }, 1000);
+
+        } else {
+            return () => {
+                clearInterval(timer);
+            };
+        }
+    }, [onPlay, time]);
+
+    const handleChange = (event: Event) => {
+        const newTime:any = event.target
+        setTime(newTime.value);
+        setFrame(frames[time].url)
+    };
+
+    function playNaParada(){
+        setOnPlay(!onPlay)
+    }
 
     return (
         <>
-            <div>
-                <span>Esses são os frames</span>
-                {frames.map((frame) =>{
-                    return <p>{frame}</p>
-                })}
-            </div>
-            <div>
-                <span>{fps}</span>
-            </div>
 
+            <img src={frame}/>
+            <Slider
+                defaultValue={0}
+                min={0}
+                max={tempoTotalDaApresentação}
+                aria-label="Default"
+                onChange={handleChange}
+                valueLabelDisplay="auto"
+                value={time}
+                style={{width:"50%"}}/>
+            <button onClick={playNaParada}>{!onPlay? "play":"stop"}</button>
+
+            <div>
+                <span>{time}</span>
+            </div>
         </>
     );
 };
